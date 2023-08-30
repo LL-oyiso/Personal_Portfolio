8553cd9b62e3ece0f940ffbdf2756fffac91e544
LL-oyiso <theo.tose.tt@gmail.com>
2023-08-30 15:52:33 +0200

    form validation for message input

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-firebase.initializeApp(firebaseConfig);
-// Initialize Firebase
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const form = document.getElementById("form");
+
+
+// catching errors before they get submitted and prevent our page from empty content 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+})
+
+
+
+
+
+
+
 // firebase.initializeApp(firebaseConfig);
+// // Initialize Firebase
+// // firebase.initializeApp(firebaseConfig);
 
-// Refernece contactInfo collections
-let contactInfo = firebase.database().ref("infos");
+// // Refernece contactInfo collections
+// let contactInfo = firebase.database().ref("infos");
 
-// Listen for a submit
-document.querySelector(".contact-form").addEventListener("submit", submitForm);
+// // Listen for a submit
+// document.querySelector(".contact-form").addEventListener("submit", submitForm);
 
-function submitForm(e) {
-  e.preventDefault();
+// function submitForm(e) {
+//   e.preventDefault();
 
-  //   Get input Values
-//   let name = document.querySelector(".name").value;
-  let email = document.querySelector(".input-field").value;
-  let message = document.querySelector(".move-textarea").value;
-  console.log( email, message);
+//   //   Get input Values
+// //   let name = document.querySelector(".name").value;
+//   let email = document.querySelector(".input-field").value;
+//   let message = document.querySelector(".move-textarea").value;
+//   console.log( email, message);
 
-  saveContactInfo( email, message);
+//   saveContactInfo( email, message);
 
-  document.querySelector(".contact-form").reset();
-}
+//   document.querySelector(".form-card").reset();
+// }
 
-// Save infos to Firebase
-function saveContactInfo( email, message) {
-  let newContactInfo = contactInfo.push();
+// // Save infos to Firebase
+// function saveContactInfo( email, message) {
+//   let newContactInfo = contactInfo.push();
 
-  newContactInfo.set({
-    // name: name,
-    email: email,
-    message: message,
-  });
-}
+//   newContactInfo.set({
+//     // name: name,
+//     email: email,
+//     message: message,
+//   });
+// }
